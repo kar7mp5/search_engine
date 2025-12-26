c9dcd3586c2113bae566ff4727a663e22d9e9c09
kar7mp5 <tommy1005a@gmail.com>
2025-12-26 14:33:35 +0900

    refactor(search_engine): Delete unusal files and refactor logger

--- search_engine/scrap_website/custom_logger.py
+++ search_engine/scrap_website/custom_logger.py
@@ -2,16 +2,23 @@ import logging
 
 
 class Logger(logging.Logger):
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.setLevel(logging.DEBUG)
         self.propagate = False
+
         # Adding a console handler
         console_handler = ConsoleHandler()
         self.addHandler(console_handler)
+        
+        # Adding a file handler
+        file_handler = ConfigHandler('./logging.log')
+        self.addHandler(file_handler)
 
 
 class ConsoleHandler(logging.StreamHandler):
+
     def __init__(self, level: int=logging.DEBUG) -> None:
         super().__init__()
         formatter = logging.Formatter(
@@ -20,3 +27,16 @@ class ConsoleHandler(logging.StreamHandler):
         )
         self.setFormatter(formatter)
         self.setLevel(level)
+
+
+class ConfigHandler(logging.FileHandler):
+    
+    def __init__(self, filename, mode = "a", encoding = None, delay = False, errors = None):
+        super().__init__(filename, mode, encoding, delay, errors)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s",
+            datefmt="%m/%d/%Y %H:%M:%S",
+        )
+        self.setFormatter(formatter)
+        self.setLevel(logging.DEBUG)
+
--- search_engine/scrap_website/views.py
+++ search_engine/scrap_website/views.py
@@ -1,46 +1,17 @@
-# crawler/views.py (full updated version to fix schema 500 error)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.renderers import BaseRenderer
-from rest_framework.permissions import AllowAny   # ← temporary, to avoid permission issues
-from django.http import HttpResponse
+from rest_framework.permissions import AllowAny
+from django.http import HttpResponse, JsonResponse
 
-from drf_spectacular.utils import extend_schema, OpenApiTypes, OpenApiExample
 from .scrap_website import ScrapWebsite
 from .serializers import CrawlRequestSerializer
 
-class CSVRenderer(BaseRenderer):
-    media_type = 'text/csv'
-    format = 'csv'
-    charset = 'utf-8'
-
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        # This won't be used since we return HttpResponse, but good to have
-        return '\n'.join(data) + '\n'
 
 class CrawlView(APIView):
-    renderer_classes = [CSVRenderer]
+
     permission_classes = [AllowAny]
-    
-    @extend_schema(
-        summary="Crawl URLs and return discovered base URLs as CSV",
-        description="Provide starting URLs, depth, and threads. Returns a CSV file download on success.",
-        request=CrawlRequestSerializer,
-        responses={
-            (200, 'text/csv'): OpenApiTypes.STR,  # For text content like CSV
-            400: OpenApiTypes.OBJECT,
-            500: OpenApiTypes.OBJECT,
-        },
-        examples=[
-            OpenApiExample(
-                name="CSV Download Example",
-                description="Sample CSV content",
-                value="https://example.com/\nhttps://another.com/\n",
-                media_type="text/csv"
-            )
-        ]
-    )
+
     def post(self, request):
         serializer = CrawlRequestSerializer(data=request.data)
         if not serializer.is_valid():
@@ -66,16 +37,23 @@ class CrawlView(APIView):
             )
 
         # Generate CSV content in-memory
-        csv_content = '\n'.join(found_bases) + '\n'
+        # csv_content = '\n'.join(found_bases) + '\n'
 
-        return HttpResponse(
-            csv_content,
-            content_type='text/csv',
-            headers={'Content-Disposition': 'attachment; filename="discovered_base_urls.csv"'}
-        )
+        # print(csv_content)
+        
+        data = {
+            'base urls': found_bases
+        }
+        
+        return JsonResponse(data)
+        
+        # return HttpResponse(
+        #     csv_content,
+        #     content_type='text/csv',
+        #     headers={'Content-Disposition': 'attachment; filename="discovered_base_urls.csv"'}
+        # )
 
 
-# Optional simple view
 from django.http import HttpResponse
 
 def members(request):
